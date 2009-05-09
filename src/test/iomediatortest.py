cddb974d75aced51fb168deca057d8801ed470f4
cdekter <cdekter@65b5f37c-86d1-11de-b610-71a7c637a048>
2009-05-09 00:57:08 +0000

    Fix for: 2782607 - Issues with '<' sign
    git-svn-id: http://autokey.googlecode.com/svn/trunk@68 65b5f37c-86d1-11de-b610-71a7c637a048

---
+++ src/test/iomediatortest.py
@@ -0,0 +1,20 @@
+import unittest
+
+from lib.iomediator import *
+
+class IoMediatorTest(unittest.TestCase):
+    
+    def testKeySplitRe(self):
+        result = KEY_SPLIT_RE.split("<ctrl>+y")
+        self.assertEqual(result, ["", "<ctrl>+", "y"])
+        
+        result = KEY_SPLIT_RE.split("asdf <ctrl>+y asdf ")
+        self.assertEqual(result, ["asdf ", "<ctrl>+", "y asdf "])
+        
+        result = KEY_SPLIT_RE.split("<table><ctrl>+y</table>")
+        self.assertEqual(result, ["", "<table>", "", "<ctrl>+", "y", "</table>", ""])
+        
+        result = KEY_SPLIT_RE.split("<!<alt_gr>+8CDATA<alt_gr>+8")
+        self.assertEqual(result, ["<!", "<alt_gr>+", "8CDATA", "<alt_gr>+", "8"])
+        
+        
