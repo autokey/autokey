5c66a18cf7e0256fe4b615ca7e42633e2d27a1e8
cdekter <cdekter@65b5f37c-86d1-11de-b610-71a7c637a048>
2009-08-19 01:42:31 +0000

    Released as v0.60.2 Update changelog Update version numbers in all files to v0.60.2 Update manpage with new wiki address
    git-svn-id: http://autokey.googlecode.com/svn/trunk@146 65b5f37c-86d1-11de-b610-71a7c637a048

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from distutils.core import setup
 
 setup(
       name="autokey",
-      version="0.60.0", 
+      version="0.60.2", 
       author="Chris Dekter",
       author_email="cdekter@gmail.com",
       url="http://autokey.googlecode.com/",
