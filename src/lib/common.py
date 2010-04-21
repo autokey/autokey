c0ed6a4604b97a24066934484e3c7b1f5e87eb10
cdekter@gmail.com <cdekter@gmail.com@65b5f37c-86d1-11de-b610-71a7c637a048>
2010-04-21 09:38:03 +0000

    * Update changelog and bump version for 0.70.3 release
    git-svn-id: http://autokey.googlecode.com/svn/trunk@274 65b5f37c-86d1-11de-b610-71a7c637a048

--- src/lib/common.py
+++ src/lib/common.py
@@ -27,7 +27,7 @@ LOG_FORMAT = "%(levelname)s - %(name)s - %(message)s"
 
 APP_NAME = "AutoKey"
 CATALOG = ""
-VERSION = "0.70.2"
+VERSION = "0.70.3"
 HOMEPAGE  = "http://autokey.googlecode.com/"
 BUG_EMAIL = "cdekter@gmail.com"
 
