29a764bbfce6861fec9719dae7bc7c733285fc34
cdekter@gmail.com <cdekter@gmail.com@65b5f37c-86d1-11de-b610-71a7c637a048>
2011-01-28 07:26:36 +0000

    * Update packaging license to GPLv3 * Bump version and update changelog for release
    git-svn-id: http://autokey.googlecode.com/svn/trunk@309 65b5f37c-86d1-11de-b610-71a7c637a048

--- src/lib/common.py
+++ src/lib/common.py
@@ -26,7 +26,7 @@ LOG_FORMAT = "%(levelname)s - %(name)s - %(message)s"
 
 APP_NAME = "AutoKey"
 CATALOG = ""
-VERSION = "0.71.1"
+VERSION = "0.71.2"
 HOMEPAGE  = "http://autokey.googlecode.com/"
 BUG_EMAIL = "cdekter@gmail.com"
 
