7a8ed54311e646cbe6266e26f7be68b1faab63c0
cdekter@gmail.com <cdekter@gmail.com@65b5f37c-86d1-11de-b610-71a7c637a048>
2011-11-22 08:56:51 +0000

    Bump up the version to 0.81.0 as this is a fairly large release
    git-svn-id: http://autokey.googlecode.com/svn/trunk@408 65b5f37c-86d1-11de-b610-71a7c637a048

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 
 setup(
       name="autokey",
-      version="0.80.4",
+      version="0.81.0",
       author="Chris Dekter",
       author_email="cdekter@gmail.com",
       url="http://autokey.googlecode.com/",
