30a1a80fed1fe4c77e30a839f62ee1177288d2f8
cdekter@gmail.com <cdekter@gmail.com@65b5f37c-86d1-11de-b610-71a7c637a048>
2011-12-03 04:01:19 +0000

    Update changelog and bump version for release
    git-svn-id: http://autokey.googlecode.com/svn/trunk@415 65b5f37c-86d1-11de-b610-71a7c637a048

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 
 setup(
       name="autokey",
-      version="0.81.1",
+      version="0.81.2",
       author="Chris Dekter",
       author_email="cdekter@gmail.com",
       url="http://autokey.googlecode.com/",
