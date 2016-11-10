b5ada00ab013eac84887fdc96112d5a1db5829e3
Troy C <troxor@users.noreply.github.com>
2016-11-09 19:57:53 -0600

    more information in release.py

--- lib/autokey-py3/release.py
+++ lib/autokey-py3/release.py
@@ -1,4 +1,8 @@
 __metaclass__ = type
 
-__version__ = '0.93.6'
-__author__  = 'Chris Dekter'
+__version__       = '0.93.6'
+__author__        = 'Chris Dekter'
+__author_email__  = 'cdekter@gmail.com'
+__maintainer__    = 'GuoCi'
+__maint_email__   = 'guociz@gmail.com'
+
