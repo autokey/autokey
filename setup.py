9886cd2aebcf8ad8e154a8dcf96a611ad7d161d0
Troy C <troxor@users.noreply.github.com>
2016-11-09 19:13:15 -0600

    autopep8 setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ long_description = ''
 try:
     with open('README.rst') as f:
         long_description = f.read()
-except FileNotFoundError: pass
+except FileNotFoundError:
+    pass
 
 MAJOR = 0
 MINOR = 93
@@ -29,38 +30,42 @@ MICRO = 6
 VERSION = '{}.{}.{}'.format(MAJOR, MINOR, MICRO)
 
 setup(
-      name="autokey-py3",
-      version=VERSION,
-      author="Chris Dekter",
-      author_email="cdekter@gmail.com",
-      maintainer='GuoCi',
-      maintainer_email='guociz@gmail.com',
-      url='https://github.com/guoci/autokey-py3',
-      # url="http://autokey.googlecode.com/",
-      license="GPL v3",
-      description="Python 3 port of AutoKey. New features have been implemented since the initial porting.",
-      long_description=long_description,
-      #py_modules=["autokey", "configurationmanager", "expansionservice", "interface",
-      #            "iomediator", "phrase", "phrasemenu", "ui"],
-      package_dir={"autokey": "src/lib"},
-      packages=["autokey", "autokey.gtkui", "autokey.qtui"],
-      package_data={"autokey.qtui" : ["data/*"],
-                    "autokey.gtkui" : ["data/*"]},
-      data_files=[("share/icons/hicolor/scalable/apps", ["config/autokey.svg", "config/autokey.png", "config/autokey-status.svg", "config/autokey-status-dark.svg", "config/autokey-status-error.svg"]),
-                ("share/icons/Humanity/scalable/apps", ["config/Humanity/autokey-status.svg", "config/Humanity/autokey-status-error.svg"]),
-                ("share/icons/ubuntu-mono-dark/apps/48", ["config/ubuntu-mono-dark/autokey-status.svg", "config/ubuntu-mono-dark/autokey-status-error.svg"]),
-                ("share/icons/ubuntu-mono-light/apps/48", ["config/ubuntu-mono-light/autokey-status.svg", "config/ubuntu-mono-light/autokey-status-error.svg"]),
-                  ("share/applications", ["config/autokey-qt.desktop", "config/autokey-gtk.desktop"]),
-                  ('share/man/man1/', ['doc/man/autokey-qt.1', 'doc/man/autokey-gtk.1', 'doc/man/autokey-run.1']),
-                  ('share/kde4/apps/autokey' , ['config/autokeyui.rc'])],
-      scripts=['autokey-qt', 'autokey-gtk', 'autokey-run', 'autokey-shell'],
-      classifiers = [
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'Intended Audience :: End Users/Desktop',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python :: 3',
-      ],
-      #packages=["plugin"]
-      )
+    name="autokey-py3",
+    version=VERSION,
+    author="Chris Dekter",
+    author_email="cdekter@gmail.com",
+    maintainer='GuoCi',
+    maintainer_email='guociz@gmail.com',
+    url='https://github.com/autokey-py3/autokey-py3',
+    # url="http://autokey.googlecode.com/",
+    license="GPL v3",
+    description="Python 3 port of AutoKey. New features have been implemented since the initial porting.",
+    long_description=long_description,
+    # py_modules=["autokey", "configurationmanager", "expansionservice", "interface",
+    #            "iomediator", "phrase", "phrasemenu", "ui"],
+    packages=["autokey", "autokey.gtkui", "autokey.qtui"],
+    package_dir={"autokey": "src/lib"},
+    package_data={"autokey.qtui": ["data/*"],
+                  "autokey.gtkui": ["data/*"]},
+    data_files=[("share/icons/hicolor/scalable/apps", ["config/autokey.svg", "config/autokey.png", "config/autokey-status.svg", "config/autokey-status-dark.svg", "config/autokey-status-error.svg"]),
+                ("share/icons/Humanity/scalable/apps",
+                 ["config/Humanity/autokey-status.svg", "config/Humanity/autokey-status-error.svg"]),
+                ("share/icons/ubuntu-mono-dark/apps/48",
+                 ["config/ubuntu-mono-dark/autokey-status.svg", "config/ubuntu-mono-dark/autokey-status-error.svg"]),
+                ("share/icons/ubuntu-mono-light/apps/48",
+                 ["config/ubuntu-mono-light/autokey-status.svg", "config/ubuntu-mono-light/autokey-status-error.svg"]),
+                ("share/applications",
+                 ["config/autokey-qt.desktop", "config/autokey-gtk.desktop"]),
+                ('share/man/man1/', ['doc/man/autokey-qt.1',
+                                     'doc/man/autokey-gtk.1', 'doc/man/autokey-run.1']),
+                ('share/kde4/apps/autokey', ['config/autokeyui.rc'])],
+    scripts=['autokey-qt', 'autokey-gtk', 'autokey-run', 'autokey-shell'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+    ]
+)
