f7bd1156ddbf20fea063701754103de0da18962c
Thomas Hess <thomas.hess@udo.edu>
2018-04-11 17:40:38 +0200

    Converted the AbbrSettings UI into a QDialog. Merged AbbrSettings into AbbrSettingsDialog.

--- lib/autokey/qtui/abbrsettings.py
+++ lib/autokey/qtui/abbrsettings.py
@@ -21,14 +21,22 @@ This dialog allows the user to set and configure abbreviations to trigger script
 """
 
 from PyQt4 import QtCore
-from PyQt4.QtGui import QListWidgetItem
+from PyQt4.QtGui import QListWidgetItem, QDialogButtonBox
 
 
 from .common import inherits_from_ui_file_with_name, EMPTY_FIELD_REGEX
+from .. import model
+
+import logging
+logger = logging.getLogger("root").getChild("Qt-GUI").getChild("Abbreviation Settings Dialog")  # type: logging.Logger
 
 
-WORD_CHAR_OPTIONS_ORDERED = ["All non-word", "Space and Enter", "Tab"]
-
+WORD_CHAR_OPTIONS = {
+                     "All non-word" : model.DEFAULT_WORDCHAR_REGEX,
+                     "Space and Enter" : r"[^ \n]",
+                     "Tab" : r"[^\t]"
+                     }
+WORD_CHAR_OPTIONS_ORDERED = tuple(sorted(WORD_CHAR_OPTIONS.keys()))
 
 class AbbrListItem(QListWidgetItem):
     """
@@ -47,7 +55,7 @@ class AbbrListItem(QListWidgetItem):
             QListWidgetItem.setData(self, role, value)
 
 
-class AbbrSettings(*inherits_from_ui_file_with_name("abbrsettings")):
+class AbbrSettingsDialog(*inherits_from_ui_file_with_name("abbrsettings")):
 
     def __init__(self, parent):
         super().__init__(parent)
@@ -56,11 +64,11 @@ class AbbrSettings(*inherits_from_ui_file_with_name("abbrsettings")):
             self.wordCharCombo.addItem(item)
 
     def setupUi(self):
-        self.setObjectName("Form")
+        self.setObjectName("Form")  # TODO: needed? Maybe use a better name than 'Form'
         super().setupUi(self)
-        QtCore.QMetaObject.connectSlotsByName(self)
 
     def on_addButton_pressed(self):
+        logger.info("New abbreviation added.")
         item = AbbrListItem("")
         self.abbrListWidget.addItem(item)
         self.abbrListWidget.editItem(item)
@@ -68,17 +76,28 @@ class AbbrSettings(*inherits_from_ui_file_with_name("abbrsettings")):
 
     def on_removeButton_pressed(self):
         item = self.abbrListWidget.takeItem(self.abbrListWidget.currentRow())
+        if item is not None:
+            logger.info("User deletes abbreviation with text: {}".format(item.text()))
         if self.abbrListWidget.count() == 0:
+            logger.debug("Last abbreviation deleted, disabling delete and OK buttons.")
             self.removeButton.setEnabled(False)
+            # The user can only accept the dialog if the content is valid
+            self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
 
     def on_abbrListWidget_itemChanged(self, item):
         if EMPTY_FIELD_REGEX.match(item.text()):
             row = self.abbrListWidget.row(item)
             self.abbrListWidget.takeItem(row)
             del item
+        else:
+            # The item is non-empty. Therefore there is at least one element in the list, thus input is valid.
+            # Allow the user to accept his edits.
+            self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(True)
 
         if self.abbrListWidget.count() == 0:
             self.removeButton.setEnabled(False)
+            # The user can only accept the dialog if the content is valid
+            self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
 
     def on_abbrListWidget_itemDoubleClicked(self, item):
         self.abbrListWidget.editItem(item)
@@ -99,3 +118,115 @@ class AbbrSettings(*inherits_from_ui_file_with_name("abbrsettings")):
         else:
             self.omitTriggerCheckbox.setEnabled(True)
             self.wordCharCombo.setEnabled(True)
+
+    def load(self, item):
+        self.targetItem = item
+        self.abbrListWidget.clear()
+
+        if model.TriggerMode.ABBREVIATION in item.modes:
+            for abbr in item.abbreviations:
+                self.abbrListWidget.addItem(AbbrListItem(abbr))
+            self.removeButton.setEnabled(True)
+            self.abbrListWidget.setCurrentRow(0)
+        else:
+            self.removeButton.setEnabled(False)
+
+        self.removeTypedCheckbox.setChecked(item.backspace)
+
+        self.__resetWordCharCombo()
+
+        wordCharRegex = item.get_word_chars()
+        if wordCharRegex in list(WORD_CHAR_OPTIONS.values()):
+            # Default wordchar regex used
+            for desc, regex in WORD_CHAR_OPTIONS.items():
+                if item.get_word_chars() == regex:
+                    self.wordCharCombo.setCurrentIndex(WORD_CHAR_OPTIONS_ORDERED.index(desc))
+                    break
+        else:
+            # Custom wordchar regex used
+            self.wordCharCombo.addItem(model.extract_wordchars(wordCharRegex))
+            self.wordCharCombo.setCurrentIndex(len(WORD_CHAR_OPTIONS))
+
+        if isinstance(item, model.Folder):
+            self.omitTriggerCheckbox.setVisible(False)
+        else:
+            self.omitTriggerCheckbox.setVisible(True)
+            self.omitTriggerCheckbox.setChecked(item.omitTrigger)
+
+        if isinstance(item, model.Phrase):
+            self.matchCaseCheckbox.setVisible(True)
+            self.matchCaseCheckbox.setChecked(item.matchCase)
+        else:
+            self.matchCaseCheckbox.setVisible(False)
+
+        self.ignoreCaseCheckbox.setChecked(item.ignoreCase)
+        self.triggerInsideCheckbox.setChecked(item.triggerInside)
+        self.immediateCheckbox.setChecked(item.immediate)
+        # Enable the OK button only if there are abbreviations in the loaded list. Otherwise only cancel is available
+        # to the user until they add a non-empty abbreviation.
+        self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(bool(self.get_abbrs()))
+
+    def save(self, item):
+        item.modes.append(model.TriggerMode.ABBREVIATION)
+        item.clear_abbreviations()
+        item.abbreviations = self.get_abbrs()
+
+        item.backspace = self.removeTypedCheckbox.isChecked()
+
+        option = str(self.wordCharCombo.currentText())
+        if option in WORD_CHAR_OPTIONS:
+            item.set_word_chars(WORD_CHAR_OPTIONS[option])
+        else:
+            item.set_word_chars(model.make_wordchar_re(option))
+
+        if not isinstance(item, model.Folder):
+            item.omitTrigger = self.omitTriggerCheckbox.isChecked()
+
+        if isinstance(item, model.Phrase):
+            item.matchCase = self.matchCaseCheckbox.isChecked()
+
+        item.ignoreCase = self.ignoreCaseCheckbox.isChecked()
+        item.triggerInside = self.triggerInsideCheckbox.isChecked()
+        item.immediate = self.immediateCheckbox.isChecked()
+
+    def reset(self):
+        self.removeButton.setEnabled(False)
+        self.abbrListWidget.clear()
+        self.__resetWordCharCombo()
+        self.omitTriggerCheckbox.setChecked(False)
+        self.removeTypedCheckbox.setChecked(True)
+        self.matchCaseCheckbox.setChecked(False)
+        self.ignoreCaseCheckbox.setChecked(False)
+        self.triggerInsideCheckbox.setChecked(False)
+        self.immediateCheckbox.setChecked(False)
+
+    def __resetWordCharCombo(self):
+        self.wordCharCombo.clear()
+        for item in WORD_CHAR_OPTIONS_ORDERED:
+            self.wordCharCombo.addItem(item)
+        self.wordCharCombo.setCurrentIndex(0)
+
+    def get_abbrs(self):
+        ret = []
+        for i in range(self.abbrListWidget.count()):
+            text = self.abbrListWidget.item(i).text()
+            ret.append(str(text))
+
+        return ret
+
+    def get_abbrs_readable(self):
+        abbrs = self.get_abbrs()
+        if len(abbrs) == 1:
+            return abbrs[0]
+        else:
+            return "[%s]" % ','.join(abbrs)
+
+    def reset_focus(self):
+        self.addButton.setFocus()
+
+    def accept(self):
+        super().accept()
+
+    def reject(self):
+        self.load(self.targetItem)
+        super().reject()
