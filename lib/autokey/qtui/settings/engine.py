autokey/autokey

commit b0f0eb6252fc66c5e2e91d6cea5b82390549c884

Qt: Merged init() methods into __init__() of Settings widgets. This was a workaround needed previously, but is not needed anymore. The Application instance is valid during the settings widget’s __init__() run now.
Added some more type hints.
Removed duplicate logger initialisation code in the Application class.

lib/autokey/qtui/settings/engine.py
@@ -43,14 +43,11 @@ class EngineSettings(*common.inherits_from_ui_file_with_name("enginesettings")):
         # It is used to reset the label to this value if a custom module path is currently set and the user deletes it.
         # Do not hard-code it to prevent possible inconsistencies.
         self.initial_folder_label_text = self.folder_label.text()
-        self.config_manager = None  # type: ConfigManager
-        self.path = None  # type: str
         self.clear_button.setEnabled(self.path is not None)
-        logger.debug("EngineSettings widget created.")
 
-    def init(self):
         self.config_manager = QApplication.instance().configManager
         self.path = self.config_manager.userCodeDir
+
         if self.config_manager.userCodeDir is not None:
             self.folder_label.setText(self.config_manager.userCodeDir)
         logger.debug("EngineSettings widget initialised, custom module search path is set to: {}".format(self.path))

lib/autokey/qtapp.py
@@ -113,8 +113,6 @@ class Application(QApplication):
         try:
             self._create_storage_directories()
             # Initialise logger
-            root_logger = logging.getLogger()
-            root_logger.setLevel(logging.DEBUG)
 
             if self._verify_not_running():
                 self._create_lock_file()

lib/autokey/qtui/dialogs/hotkeysettings.py
@@ -22,10 +22,11 @@ from PyQt5.QtWidgets import QDialogButtonBox
 
 from autokey.qtui import common as ui_common
 
-from autokey import iomediator, model
+from autokey import iomediator, model, configmanager as cm
 from autokey.iomediator.key import Key
 
 logger = ui_common.logger.getChild("Hotkey Settings Dialog")  # type: logging.Logger
+Item = typing.Union[model.Folder, model.Script, model.Phrase]
 
 
 class HotkeySettingsDialog(*ui_common.inherits_from_ui_file_with_name("hotkeysettings")):
@@ -52,7 +53,7 @@ class HotkeySettingsDialog(*ui_common.inherits_from_ui_file_with_name("hotkeyset
         self.recording_finished.connect(self.setButton.setEnabled)
         self._key = ""
         self.key = None  # Use the property setter to emit the key_assigned signal and disable the Ok button.
-        self.target_item = None
+        self.target_item = None  # type: Item
         self.grabber = None  # type: iomediator.KeyGrabber
 
     @property
@@ -67,14 +68,14 @@ class HotkeySettingsDialog(*ui_common.inherits_from_ui_file_with_name("hotkeyset
     def on_setButton_pressed(self):
         """
         Start recording a key combination when the user clicks on the setButton.
-        The button itself- is automatically disabled during the recording process.
+        The button itself is automatically disabled during the recording process.
         """
         self.keyLabel.setText("Press a key or combination...")  # TODO: i18n
         logger.debug("User starts to record a key combination.")
         self.grabber = iomediator.KeyGrabber(self)
         self.grabber.start()
 
-    def load(self, item):
+    def load(self, item: Item):
         self.target_item = item
         if model.TriggerMode.HOTKEY in item.modes:
             self.controlButton.setChecked(Key.CONTROL in item.modifiers)
@@ -175,7 +176,8 @@ class HotkeySettingsDialog(*ui_common.inherits_from_ui_file_with_name("hotkeyset
 
 class GlobalHotkeyDialog(HotkeySettingsDialog):
 
-    def load(self, item):
+    def load(self, item: cm.GlobalHotkey):
+        print("GlobalHotkeyDialog: item type: " + str(type(item)))
         self.target_item = item
         if item.enabled:
             self.controlButton.setChecked(Key.CONTROL in item.modifiers)
@@ -196,7 +198,7 @@ class GlobalHotkeyDialog(HotkeySettingsDialog):
         else:
             self.reset()
 
-    def save(self, item):
+    def save(self, item: cm.GlobalHotkey):
         # Build modifier list
         modifiers = self.build_modifiers()
 

lib/autokey/qtui/settings/settingsdialog.py
@@ -33,8 +33,6 @@ class SettingsDialog(*common.inherits_from_ui_file_with_name("settingsdialog")):
     def __init__(self, parent: QWidget=None):
         super(SettingsDialog, self).__init__(parent)
         self.setupUi(self)
-        self.special_hotkeys_page.init()
-        self.script_engine_page.init()
         logger.info("Settings dialog window created.")
 
     @pyqtSlot()  # Avoid the slot being called twice, by both signals clicked() and clicked(bool).

lib/autokey/qtui/settings/specialhotkeys.py
@@ -16,7 +16,7 @@
 
 import typing
 
-from PyQt5.QtWidgets import QDialog, QWidget, QApplication
+from PyQt5.QtWidgets import QDialog, QWidget, QApplication, QLabel, QPushButton
 
 from autokey.qtui.dialogs import GlobalHotkeyDialog
 import autokey.qtui.common as ui_common
@@ -24,7 +24,6 @@ import autokey.qtui.common as ui_common
 if typing.TYPE_CHECKING:
     import logging
 
-    from autokey.configmanager import ConfigManager
     from autokey.qtapp import Application
 
 logger = ui_common.logger.getChild("Special Hotkey Settings widget")  # type: logging.Logger
@@ -45,14 +44,11 @@ class SpecialHotkeySettings(*ui_common.inherits_from_ui_file_with_name("specialh
         super(SpecialHotkeySettings, self).__init__(parent)
         self.setupUi(self)
 
-        self.config_manager = None  # type: ConfigManager
 
         self.show_config_dlg = GlobalHotkeyDialog(parent)
         self.toggle_monitor_dlg = GlobalHotkeyDialog(parent)
         self.use_config_hotkey = False
         self.use_service_hotkey = False
-
-    def init(self):
         app = QApplication.instance()  # type: Application
         self.config_manager = app.configManager
         self.use_config_hotkey = self._load_hotkey(self.config_manager.configHotkey, self.config_key_label,
@@ -60,8 +56,9 @@ class SpecialHotkeySettings(*ui_common.inherits_from_ui_file_with_name("specialh
         self.use_service_hotkey = self._load_hotkey(self.config_manager.toggleServiceHotkey, self.monitor_key_label,
                                                     self.toggle_monitor_dlg, self.clear_monitor_button)
 
+
     @staticmethod
-    def _load_hotkey(item, label, dialog, clear_button):
+    def _load_hotkey(item, label: QLabel, dialog: GlobalHotkeyDialog, clear_button: QPushButton):
         dialog.load(item)
         if item.enabled:
             key = item.hotKey

commit 18bdef2641c5da2b8edf0e0e96de2c33f43e5198

Fix issues introduced in b0f0eb6252fc66c5e2e91d6cea5b82390549c884

lib/autokey/qtui/settings/engine.py
@@ -43,10 +43,10 @@ class EngineSettings(*common.inherits_from_ui_file_with_name("enginesettings")):
         # It is used to reset the label to this value if a custom module path is currently set and the user deletes it.
         # Do not hard-code it to prevent possible inconsistencies.
         self.initial_folder_label_text = self.folder_label.text()
-        self.clear_button.setEnabled(self.path is not None)
 
         self.config_manager = QApplication.instance().configManager
         self.path = self.config_manager.userCodeDir
+        self.clear_button.setEnabled(self.path is not None)
 
         if self.config_manager.userCodeDir is not None:
             self.folder_label.setText(self.config_manager.userCodeDir)
@@ -74,7 +74,7 @@ class EngineSettings(*common.inherits_from_ui_file_with_name("enginesettings")):
         PyQt slot called when the user hits the "Browse" button.
         Display a directory selection dialog and store the returned path.
         """
-        path = QFileDialog.getExisingDirectory(self.parentWidget(), "Choose a directory containing Python modules")
+        path = QFileDialog.getExistingDirectory(self.parentWidget(), "Choose a directory containing Python modules")
 
         if path:  # Non-empty means the user chose a path and clicked on OK
             self.path = path