e89af1ef316f92638f451285bb377a4c848785d7
Thomas Hess <thomas.hess@udo.edu>
2019-03-31 14:38:43 +0200

    GTK Tray icon: Prevent errors when using the view script error button.
    If the main window is closed, the accessed UI components are currently
    unavailable (None). This causes AttributeErrors and a non-working
    menu entry.
    This workaround simply opens the main window before showing the dialogue
    window to ensure that the components are available, when accessed.
    
    Closes #222.

--- lib/autokey/gtkui/notifier.py
+++ lib/autokey/gtkui/notifier.py
@@ -130,6 +130,7 @@ class IndicatorNotifier:
         pass
         
     def on_show_error(self, widget, data=None):
+        self.on_show_configure(widget, data)
         self.app.show_script_error(self.app.configWindow.ui)
         self.errorItem.hide()
         self.update_visible_status()
