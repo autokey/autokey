5bb16fbd6d2afc8def7adac67cb64c6ef82641dd
bluedrink9 <bluedrink9@github.com>
2019-12-12 16:26:25 +1300

    Add abstract methods to macro classes

--- lib/autokey/macro.py
+++ lib/autokey/macro.py
@@ -31,20 +31,20 @@ else:
 
 
 class MacroManager:
-    
+
     def __init__(self, engine):
         self.macros = []
-        
+
         self.macros.append(ScriptMacro(engine))
         self.macros.append(DateMacro())
         self.macros.append(FileContentsMacro())
         self.macros.append(CursorMacro())
-        
+
     def get_menu(self, callback, menu=None):
         if common.USING_QT:
             for macro in self.macros:
                 menu.addAction(MacroAction(menu, macro, callback))
-        
+
         else:
             menu = Gtk.Menu()
 
@@ -56,17 +56,30 @@ class MacroManager:
             menu.show_all()
 
         return menu
-        
+
     def process_expansion(self, expansion):
         parts = KEY_SPLIT_RE.split(expansion.string)
-        
-        for macro in self.macros:        
+
+        for macro in self.macros:
             macro.process(parts)
-        
+
         expansion.string = ''.join(parts)
-        
+
 
 class AbstractMacro:
+
+    @property
+    @abstractmethod
+    def ID(self):
+        pass
+    @property
+    @abstractmethod
+    def TITLE(self):
+        pass
+    @property
+    @abstractmethod
+    def ARGS(self):
+        pass
 
     def get_token(self):
         ret = "<%s" % self.ID
@@ -74,17 +87,18 @@ class AbstractMacro:
         ret += "".join((" " + k + "=" for k, v in self.ARGS))
         ret += ">"
         return ret
-            
+
     def _can_process(self, token):
         if KEY_SPLIT_RE.match(token):
             return token[1:-1].split(' ', 1)[0] == self.ID
         else:
             return False
-        
+
     def _get_args(self, token):
+        print(token)
         l = token[:-1].split(' ')
         ret = {}
-                
+
         if len(l) > 1:
             for arg in l[1:]:
                 key, val = arg.split('=', 1)
@@ -93,10 +107,11 @@ class AbstractMacro:
         for k, v in self.ARGS:
             if k not in ret:
                 raise Exception("Missing mandatory argument '{}' for macro '{}'".format(k, self.ID))
-        
+
         return ret
-        
+
     def process(self, parts):
+        print("parts ", parts)
         for i in range(len(parts)):
             if self._can_process(parts[i]):
                 self.do_process(parts, i)
@@ -111,7 +126,7 @@ class CursorMacro(AbstractMacro):
     ID = "cursor"
     TITLE = _("Position cursor")
     ARGS = []
-    
+
     def do_process(self, parts, i):
         try:
             lefts = len(''.join(parts[i+1:]))
@@ -119,18 +134,18 @@ class CursorMacro(AbstractMacro):
             parts[i] = ''
         except IndexError:
             pass
-                        
-    
+
+
 class ScriptMacro(AbstractMacro):
 
     ID = "script"
     TITLE = _("Run script")
     ARGS = [("name", _("Name")),
             ("args", _("Arguments (comma separated)"))]
-    
+
     def __init__(self, engine):
         self.engine = engine
-    
+
     def do_process(self, parts, i):
         args = self._get_args(parts[i])
         self.engine.run_script_from_macro(args)
@@ -142,7 +157,7 @@ class DateMacro(AbstractMacro):
     ID = "date"
     TITLE = _("Insert date")
     ARGS = [("format", _("Format"))]
-    
+
     def do_process(self, parts, i):
         format_ = self._get_args(parts[i])["format"]
         date = datetime.datetime.now().strftime(format_)
@@ -154,9 +169,9 @@ class FileContentsMacro(AbstractMacro):
     ID = "file"
     TITLE = _("Insert file contents")
     ARGS = [("name", _("File name"))]
-    
+
     def do_process(self, parts, i):
         name = self._get_args(parts[i])["name"]
-        
+
         with open(name, "r") as inputFile:
             parts[i] = inputFile.read()
