7345e1eda36abf8a67a545e4883e7d999f54ce04
bluedrink9 <bluedrink9@github.com>
2019-12-23 14:29:34 +1300

    Comment

--- lib/autokey/gtkapp.py
+++ lib/autokey/gtkapp.py
@@ -45,7 +45,10 @@ import autokey.configmanager.configmanager as cm
 import autokey.configmanager.configmanager_constants as cm_constants
 
 
-PROGRAM_NAME = _("AutoKey")  # TODO: where does this _ named function come from? It must be one of those from x import *
+# TODO: this _ named function is initialised by gettext.install(), which is for
+# localisation. It marks the string as a candidate for translation, but I don't
+# know what else.
+PROGRAM_NAME = _("AutoKey")
 DESCRIPTION = _("Desktop automation utility")
 COPYRIGHT = _("(c) 2008-2011 Chris Dekter")
 
