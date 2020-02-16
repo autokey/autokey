a3d098358d7356c299cd40c37517038e2718c226
Thomas Hess <thomas.hess@udo.edu>
2020-02-16 20:55:52 +0100

    Fix missing import

--- lib/autokey/qtui/dialogs/windowfiltersettings.py
+++ lib/autokey/qtui/dialogs/windowfiltersettings.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import re
+
 from PyQt5.QtWidgets import QDialog
 
 from autokey.qtui import common as ui_common
