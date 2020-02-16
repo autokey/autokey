c0c2e2939f2352a1d3d83c8edafc9007848d6a1e
Thomas Hess <thomas.hess@udo.edu>
2020-02-16 21:27:48 +0100

    Release version 0.95.10

--- lib/autokey/common.py
+++ lib/autokey/common.py
@@ -40,7 +40,7 @@ LOG_FORMAT = "%(asctime)s %(levelname)s - %(name)s - %(message)s"
 
 APP_NAME = "autokey"
 CATALOG = ""
-VERSION = "0.95.9"
+VERSION = "0.95.10"
 HOMEPAGE = "https://github.com/autokey/autokey"
 AUTHOR = 'Chris Dekter'
 AUTHOR_EMAIL = 'cdekter@gmail.com'
