37a69c1bcd91c402e040cdcf6927cc9bc6e72494
Thomas Hess <thomas.hess@udo.edu>
2020-03-22 23:36:06 +0100

    Scripting: fix system.exec_command() crashing on empty process output.
    Fixes #379

--- lib/autokey/scripting.py
+++ lib/autokey/scripting.py
@@ -494,9 +494,10 @@ class System:
                     stdout=subprocess.PIPE,
                     universal_newlines=True) as p:
                 output = p.communicate()[0]
-                if output[-1] == "\n":
+                if output.endswith("\n"):
                     # Most shell output has a new line at the end, which we
-                    # don't want. Drop the trailing newline character
+                    # don't want. Drop the trailing newline character,
+                    # if the command output something that ends on "\n"
                     output = output[:-1]
                 if p.returncode:
                     raise subprocess.CalledProcessError(p.returncode, output)
