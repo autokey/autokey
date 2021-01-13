b0350ba8d0098f9a717618cc71e6f51aeba43281
bluedrink9 <bluedrink9@github.com>
2021-01-14 12:40:11 +1300

    Add file patch funcion to test

--- tests/UIs/test_gtk.py
+++ tests/UIs/test_gtk.py
@@ -21,6 +21,7 @@ import pytest
 skip = pytest.mark.skip
 import hamcrest as hm
 
+import autokey
 import autokey.gtkapp as gtkui
 
 def ret_arg(arg):
@@ -35,6 +36,40 @@ def ret_arg(arg):
 def get_errors_in_log(caplog):
     errors = [record for record in caplog.get_records('call') if record.levelno >= logging.ERROR]
     return errors
+
+# Automatically patch config dirs wherever needed, so that tests don't
+# overwrite or rely on user config.
+@pytest.fixture(scope="function", autouse=True)
+def create_file_patches(tmp_path, request):
+    patches = []
+    # Patching relies on namespaces. Since configmanager uses from
+    # ..constants import X, we have to use configmanager's namespace
+    # as well as of common.
+    patch_paths = {
+        'CONFIG_DIR': str(tmp_path),
+        'CONFIG_DEFAULT_FOLDER': str(tmp_path / "data"), 
+        'CONFIG_FILE': str(tmp_path / "autokey.json"),
+        'CONFIG_FILE_BACKUP': str(tmp_path / "autokey.json~"),
+        'LOCK_FILE': str(tmp_path / "autokey.pid"),
+    }
+    namespaces = ['autokey.common', 'autokey.configmanager.configmanager']
+
+    for name in namespaces:
+        for patched, path in patch_paths.items():
+            patches.append(
+                patch(name + '.' + patched, path)
+            )
+    for p in patches:
+        try:
+            p.__enter__()
+        except AttributeError:
+            continue
+
+    def unpatch():
+        for p in patches:
+            p.__exit__()
+    request.addfinalizer(unpatch)
+
 
 
 @patch('sys.argv', ['autokey-app-testing'])
