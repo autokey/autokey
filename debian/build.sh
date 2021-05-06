fd4d05a5388f0efabcb5508d6b09de015821b129
bluedrink9 <bluedrink9@users.noreply.github.com>
2021-05-06 18:04:22 +1200

    Fix deb build getting wrong version number

--- debian/build.sh
+++ debian/build.sh
@@ -4,9 +4,11 @@
   cat debian/build_requirements.txt | xargs sudo apt install -y
 
 VERSION=$(git describe --tags --abbrev=0 --match "v*.*.*")
+# Strip leading 'v' because that is invalid as a debian version number
+DEBVERSION="${VERSION#?}"
 uscan -dd
 # Create fake changelog entry just to get the correct version number on the deb
-dch --newversion "$VERSION" ""
+dch --newversion "$DEBVERSION" ""
 dpkg-buildpackage -i --build=binary --unsigned-source
 
 # # Update PPA. Requires more in-depth packaging.
