3bba2c5216974bf677e747b381c77bf7930fc63a
bluedrink9 <bluedrink9@github.com>
2021-09-08 11:20:59 +1200

    Feat: Use new xdo interface in interface

--- lib/autokey/interface.py
+++ lib/autokey/interface.py
@@ -29,7 +29,7 @@ import select
 import queue
 import subprocess
 import time
-import xdo
+import traceback
 
 import autokey.model.phrase
 if typing.TYPE_CHECKING:
@@ -37,6 +37,7 @@ if typing.TYPE_CHECKING:
 import autokey.configmanager.configmanager_constants as cm_constants
 from autokey.sys_interface.abstract_interface import AbstractSysInterface, AbstractMouseInterface, AbstractWindowInterface, AbstractSysKeyOutputInterface
 from autokey.sys_interface.clipboard import Clipboard
+from autokey.sys_interface.xdo_interface import XdoSendInterface
 
 
 # Imported to enable threading in Xlib. See module description. Not an unused import statement.
@@ -124,7 +125,7 @@ class XInterfaceBase(threading.Thread, AbstractMouseInterface, AbstractWindowInt
         self.lastChars = [] # QT4 Workaround
         self.__enableQT4Workaround = False # QT4 Workaround
         self.shutdown = False
-        self.doer = xdo.Xdo()
+        self.xdo = XdoSendInterface(app, mediator)
 
         # Event loop
         self.eventThread = threading.Thread(target=self.__eventLoop)
@@ -342,18 +343,6 @@ class XInterfaceBase(threading.Thread, AbstractMouseInterface, AbstractWindowInt
                 logger.exception("Error in X event loop thread: {}".format(e))
 
             self.queue.task_done()
-
-    def xdowrite_delayed(self, text: str, delay: int=12*1000, start_delay:float=1):
-        """
-        Types text, using inter-key delay delay in μs,
-        Wait start_delay seconds before typing to allow switching
-        between windows after hitting enter in the interactive prompt.
-        """
-        time.sleep(start_delay)
-        print(f"Send {text}")
-        current_window = self.doer.get_focused_window_sane()
-        print(f"Current window={current_window}")
-        self.doer.enter_text_window(current_window, text.encode("utf-8"), delay=delay)
 
     def __enqueue(self, method: typing.Callable, *args):
         self.queue.put_nowait((method, args))
@@ -721,6 +710,12 @@ class XInterfaceBase(threading.Thread, AbstractMouseInterface, AbstractWindowInt
         Send a string of printable characters.
         """
         logger.debug("Sending string: %r", string)
+        try:
+            self.xdo.send_string(string)
+            return
+        except Exception as e:
+            logger.error("Error sending string via xdo: {}".format(e))
+            traceback.print_exc()
         # Determine if workaround is needed
         if not cm.ConfigManager.SETTINGS[cm_constants.ENABLE_QT4_WORKAROUND]:
             self.__checkWorkaroundNeeded()
@@ -801,7 +796,13 @@ class XInterfaceBase(threading.Thread, AbstractMouseInterface, AbstractWindowInt
 
     def __sendKey(self, keyName):
         logger.debug("Send special key: [%r]", keyName)
-        self.__sendKeyCode(self.__lookupKeyCode(keyName))
+        logger.debug("Send special key: [{}]".format(keyName))
+        try:
+            self.xdo.send_key(keyName)
+            return
+        except Exception as e:
+            logger.error("Error sending key via xdo: {}".format(e))
+            traceback.print_exc()
 
     def __fakeKeypress(self, keyName):
         keyCode = self.__lookupKeyCode(keyName)
@@ -818,6 +819,12 @@ class XInterfaceBase(threading.Thread, AbstractMouseInterface, AbstractWindowInt
 
     def __sendModifiedKey(self, keyName, modifiers):
         logger.debug("Send modified key: modifiers: %s key: %s", modifiers, keyName)
+        try:
+            self.xdo.send_modified_key(keyName)
+            return
+        except Exception as e:
+            logger.error("Error sending modified key via xdo: {}".format(e))
+            traceback.print_exc()
         try:
             keyCode = self.__lookupKeyCode(keyName)
             self.__send_keycode_with_modifiers_pressed(keyCode,
