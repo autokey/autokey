075c581798697d2065c4f5eadb9c58b75887a587
bluedrink9 <bluedrink9@github.com>
2021-09-08 11:20:34 +1200

    Feat: Implement xdo_interface with proper send methods

--- lib/autokey/sys_interface/xdo_interface.py
+++ lib/autokey/sys_interface/xdo_interface.py
@@ -34,75 +34,91 @@ from autokey.sys_interface.abstract_interface import AbstractSysInterface, Abstr
 from autokey import common
 
 
-# Imported to enable threading in Xlib. See module description. Not an unused import statement.
-import Xlib.threaded as xlib_threaded
-# Delete again, as the reference is not needed anymore after the import side-effect has done it’s work.
-# This (hopefully) also prevents automatic code cleanup software from deleting an "unused" import and re-introduce
-# issues.
-del xlib_threaded
-
-from Xlib.error import ConnectionClosedError
-
-
-if common.USED_UI_TYPE == "GTK":
-    import gi
-    gi.require_version('Gtk', '3.0')
-    try:
-        gi.require_version('Atspi', '2.0')
-        import pyatspi
-        HAS_ATSPI = True
-    except ImportError:
-        HAS_ATSPI = False
-    except ValueError:
-        HAS_ATSPI = False
-    except SyntaxError:  # pyatspi 2.26 fails when used with Python 3.7
-        HAS_ATSPI = False
-
 logger = __import__("autokey.logger").logger.get_logger(__name__)
-
-def str_or_bytes_to_bytes(x: typing.Union[str, bytes, memoryview]) -> bytes:
-    if type(x) == bytes:
-        # logger.info("using LiuLang's python3-xlib")
-        return x
-    if type(x) == str:
-        logger.debug("using official python-xlib")
-        return x.encode("utf8")
-    if type(x) == memoryview:
-        logger.debug("using official python-xlib")
-        return x.tobytes()
-    raise RuntimeError("x must be str or bytes or memoryview object, type(x)={}, repr(x)={}".format(type(x), repr(x)))
-
 
 # This tuple is used to return requested window properties.
 WindowInfo = typing.NamedTuple("WindowInfo", [("wm_title", str), ("wm_class", str)])
 
-
-class XdoSendInterface(threading.Thread, AbstractSysKeyOutputInterface):
+class XdoSendInterface(AbstractSysKeyOutputInterface):
+# class XdoSendInterface(threading.Thread, AbstractSysKeyOutputInterface):
 
     def __init__(self, mediator, app):
-        threading.Thread.__init__(self)
-        self.setDaemon(True)
-        self.setName("XdoSendInterface-thread")
+        # threading.Thread.__init__(self)
+        # self.setDaemon(True)
+        # self.setName("XdoSendInterface-thread")
         self.doer = xdo.Xdo()
         self.mediator = mediator  # type: IoMediator
         self.app = app
-        self.shutdown = False
+        # Uses XQueryKeymap
+        # self.doer.get_active_keys_to_keycode_list()
 
-        self.eventThread = threading.Thread(target=self.__eventLoop)
-        self.queue = queue.Queue()
-
-        # Event listener
-        self.listenerThread = threading.Thread(target=self.__flush_events_loop)
-        self.clipboard = Clipboard()
-
-    def write_delayed(self, text: str, delay: int=12*1000, start_delay:float=1):
+    def send_string(self, string, delay: int=5*1000):
         """
-        Types text, using inter-key delay delay in μs,
+        Types text, using inter-key delay `delay` in μs,
         Wait start_delay seconds before typing to allow switching
         between windows after hitting enter in the interactive prompt.
         """
-        time.sleep(start_delay)
-        print(f"Send {text}")
         current_window = self.doer.get_focused_window_sane()
-        print(f"Current window={current_window}")
-        self.doer.enter_text_window(current_window, text.encode("utf-8"), delay=delay)
+        self.__pause_autokey()
+        self.doer.enter_text_window(current_window, string.encode("utf-8"), delay=delay)
+
+    def send_key(self, key_name, delay: int=2*1000):
+        """
+        This allows you to send keysequences by symbol name. Any combination of
+        X11 KeySym names separated by ‘+’ are valid. Single KeySym names are
+        valid, too.
+
+        Examples:
+        “l” “semicolon” “alt+Return” “Alt_L+Tab”
+        """
+        current_window = self.doer.get_focused_window_sane()
+        self.__pause_autokey()
+        key = self.__convert_key_name_autokey_to_xdo(key_name)
+        self.doer.send_keysequence_window(current_window, key, delay=delay)
+
+    def send_modified_key(self, key_name, delay: int=2*1000):
+        self.send_key(key_name, delay)
+
+    def press_key(self, key_name, delay: int=5*1000):
+        self.__press_or_release_key(key_name, delay, press=True)
+    def release_key(self, key_name, delay: int=5*1000):
+        self.__press_or_release_key(key_name, delay, press=False)
+
+    def fake_keypress(self, key_name):
+        pass
+    def fake_keydown(self, key_name):
+        pass
+    def fake_keyup(self, key_name):
+        pass
+
+    def __press_or_release_key(self, key_name, delay: int=5*1000, press=True):
+        current_window = self.doer.get_focused_window_sane()
+        self.__pause_autokey()
+        key = self.__convert_key_name_autokey_to_xdo(key_name)
+        if press:
+            self.doer.send_keysequence_window_down(current_window, key, delay=delay)
+        else:
+            self.doer.send_keysequence_window_up(current_window, key, delay=delay)
+
+    def __convert_key_name_autokey_to_xdo(self, key_name):
+        """
+        At this stage, we rely on the assumption that all keys are sent as valid X11 KeySyms, only the modifiers are surrounded by <>
+        """
+        key_name =key_name.replace("<", "")
+        key_name =key_name.replace(">", "")
+        return key_name
+
+
+    def __pause_autokey(self):
+        """
+        Xdo works at a higher level than the rest of the xrecord interface that autokey uses.
+        This causes a couple of problems. It means that if xrecord has grabbed the keyboard to prevent input, xdo cannot input anything.
+        It also means that xdo key events are seen by autokey as user key events.
+        To avoid that, we ungrab the keyboard and pause responding to keyboard events while using xdo commands.
+        """
+        self.app.pause_service()
+        self.mediator.finish_send()
+        yield
+        self.mediator.begin_send()
+        self.app.unpause_service()
+
