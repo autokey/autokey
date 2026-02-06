0d34a5baff7862c1168493dd622aa4ec4299573d
adventuretc <a@a.com>
2026-02-06 14:14:46 +0100

    Added contributor names

--- tests/scripting_api/test_engine.py
+++ tests/scripting_api/test_engine.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2019 Thomas Hess <thomas.hess@udo.edu>
+# Copyright (C) 2026 Bertalan Göller <intmianol@disroot.org>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
