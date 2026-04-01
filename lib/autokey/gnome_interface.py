be92c5a0ff4740a2b3c5356d6467a8b259f11705
dlk3 <dave@daveking.com>
2026-04-01 11:55:22 -0400

    Remove copyright block at the request of the project maintainers

--- lib/autokey/gnome_interface.py
+++ lib/autokey/gnome_interface.py
@@ -1,21 +1,3 @@
-#  Copyright (C) 2023  @sebastiansam55 on GitHub.com
-#  Copyright (C) 2026  David King <dave@daveking.com>
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License,
-#  version 2, as published by the Free Software Foundation.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License,
-#  version 2, along with this program; if not, see 
-#  <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
-#
-#####################################################################
-
 import dbus
 import json
 from dbus.mainloop.glib import DBusGMainLoop
