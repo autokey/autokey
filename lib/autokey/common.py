7033fa13106f1cb52f223721900f0c424660a5e5
Elliria <Elliria@users.noreply.github.com>
2026-05-19 01:53:16 -0400

    Fix version format in common.py

--- lib/autokey/common.py
+++ lib/autokey/common.py
@@ -35,7 +35,7 @@ LOCK_FILE = os.path.join(RUN_DIR, "autokey.pid")
 
 APP_NAME = "autokey"
 CATALOG = ""
-VERSION = "0.97.0~beta0"
+VERSION = "0.97.0-beta.0"
 HOMEPAGE = "https://github.com/autokey/autokey"
 AUTHOR = 'Chris Dekter'
 AUTHOR_EMAIL = 'cdekter@gmail.com'
