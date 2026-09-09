c436033c414b2251e2861e5756b71430bdfac64b
Chris Lee <github@chrislee.dhs.org>
2026-09-09 06:58:25 -0400

    Drop non-standard copyright header from new test files
    Every existing copyright header in this codebase attributes to a real
    individual contributor (Chris Dekter, Thomas Hess, BlueDrink9, Sam
    Sebastian, David King) -- a generic "AutoKey contributors" line, as
    these files had, appears nowhere else. A large share of files
    (notably uinput_interface.py and keygrabber.py, the two files these
    tests exercise) have no copyright header at all, which is an equally
    established convention here. Dropping it rather than attributing to a
    name, pending a maintainer call on how contributions like this should
    be credited going forward.

--- tests/test_uinput_interface.py
+++ tests/test_uinput_interface.py
@@ -1,10 +1,3 @@
-# Copyright (C) 2026 AutoKey contributors
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
 """
 Unit tests for the mouse-button handling in uinput_interface.py.
 
